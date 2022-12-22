1ead359251783836d491cc10f98fc17c334452d3
Valentin Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2022-12-22 18:55:25 +0100

    22122022-1 : array search

--- scripts/utils/recipeSearch.js
+++ scripts/utils/recipeSearch.js
@@ -1,5 +1,11 @@
 // This file will be different depending on the branch you're on.
 function getFilteredRecipes(e,searchedRecipe) {
-    // On this main/master branch, it does nothing.
-    return recipes;
+    // On this "array" branch, we're using the array forEach method
+    let matchingRecipes = [];
+    recipes.forEach(function (recipe) {
+        if (recipe.name.toLowerCase().includes(searchedRecipe.toLowerCase())) {
+            matchingRecipes.push(recipe);
+        }
+    })
+    return matchingRecipes;
 }
