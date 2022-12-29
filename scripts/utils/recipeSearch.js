3b0c4a401c9648810d3f7d74566dc23b2d655657
Valentin Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2022-12-29 21:48:18 +0100

    29122022-5 : better filter

--- scripts/utils/recipeSearch.js
+++ scripts/utils/recipeSearch.js
@@ -1,11 +1,25 @@
 // This file will be different depending on the branch you're on.
-function getFilteredRecipes(searchedRecipe) {
+function getFilteredRecipes(searchedRecipe, currentRecipes=recipes) {
     // On this "native" branch, we're using the native loops and methods
     let matchingRecipes = [];
-    for (recipe of recipes) {
+    for (recipe of currentRecipes) {
         if (recipe.name.toLowerCase().includes(searchedRecipe.toLowerCase())) {
+            matchingRecipes.push(recipe);
+        } else
+        if (recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(searchedRecipe.toLowerCase()))) {
+            matchingRecipes.push(recipe);
+        } else
+        if (recipe.description.toLowerCase().includes(searchedRecipe.toLowerCase())) {
             matchingRecipes.push(recipe);
         }
     }
     return matchingRecipes;
-}
+}if (recipe.name.toLowerCase().includes(searchedRecipe.toLowerCase())) {
+            matchingRecipes.push(recipe);
+        } else
+        if (recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(searchedRecipe.toLowerCase()))) {
+            matchingRecipes.push(recipe);
+        } else
+        if (recipe.description.toLowerCase().includes(searchedRecipe.toLowerCase())) {
+            matchingRecipes.push(recipe);
+        }
